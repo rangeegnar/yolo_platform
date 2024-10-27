0d1ffc16afba158d3d7b667e4a8256f8b0011e73
zhao <2466679442@qq.com>
2024-10-27 18:54:42 +0800

    'index生成'

--- web/views/home.py
+++ web/views/home.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from tracer import settings
 
 def index(request):
-    return render(request, 'index.html')
+    index_video_url = f"{settings.MEDIA_URL}AAA_index/range_index.mp4"
+    return render(request, 'index.html', {'index_video_url': index_video_url})
